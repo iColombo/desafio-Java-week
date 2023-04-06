e45f4be8cb2fa77c9bf4e729a121f2d86ccdd5c6
iColombo <78855330+iColombo@users.noreply.github.com>
2023-04-06 03:34:26 +0100

    Alignments in Controller.

--- src/main/java/com/linuxtips/challengejavaweek/controller/ProdutoController.java
+++ src/main/java/com/linuxtips/challengejavaweek/controller/ProdutoController.java
@@ -25,35 +25,35 @@ public class ProdutoController {
     }
 
     @PostMapping
-	public ResponseEntity<Object> createProduto(@RequestBody @Valid ProdutoDto produtoDto) {	
-		if (this.produtoService.existsByNome(produtoDto.getNome()))
-			return ResponseEntity.status(HttpStatus.CONFLICT).body("Dados repetidos: O Produto já existe!");			
-		
-		var produto = new Produto();		
-		BeanUtils.copyProperties(produtoDto, produto);
-		
-		return ResponseEntity.status(HttpStatus.CREATED).body(this.produtoService.criarProduto(produto));
-	}
+    public ResponseEntity<Object> createProduto(@RequestBody @Valid ProdutoDto produtoDto) {	
+	if (this.produtoService.existsByNome(produtoDto.getNome()))
+	    return ResponseEntity.status(HttpStatus.CONFLICT).body("Dados repetidos: O Produto já existe!");			
+	
+	var produto = new Produto();		
+	BeanUtils.copyProperties(produtoDto, produto);
+	
+	return ResponseEntity.status(HttpStatus.CREATED).body(this.produtoService.criarProduto(produto));
+    }
     
-	@GetMapping
-	public ResponseEntity<List<Produto>> readProdutos() {
-		return ResponseEntity.status(HttpStatus.OK).body(this.produtoService.listarProdutos());
-	}
+    @GetMapping
+    public ResponseEntity<List<Produto>> readProdutos() {
+	return ResponseEntity.status(HttpStatus.OK).body(this.produtoService.listarProdutos());
+    }
 
-	@GetMapping("/{id}")
-	public ResponseEntity<Object> getPais(@PathVariable(value = "id") Long id) {
-		return ResponseEntity.status(HttpStatus.OK).body(this.produtoService.buscarProdutoPeloId(id));
-	}
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getPais(@PathVariable(value = "id") Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.produtoService.buscarProdutoPeloId(id));
+    }
 
     @PutMapping("/{id}")
-	public ResponseEntity<Object> updatePais(@PathVariable(value = "id") Long id, 
-													@RequestBody @Valid Produto produto) {
-		return ResponseEntity.status(HttpStatus.OK).body(this.produtoService.atualizarProdutoPeloId(produto, id));
-	}
+    public ResponseEntity<Object> updatePais(@PathVariable(value = "id") Long id, 
+	  				@RequestBody @Valid Produto produto) {
+	return ResponseEntity.status(HttpStatus.OK).body(this.produtoService.atualizarProdutoPeloId(produto, id));
+    }
 
     @DeleteMapping("/{id}")
-	public ResponseEntity<Object> deletePais(@PathVariable(value = "id") Long id) {
-		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(this.produtoService.apagarPeloId(id));
-	}
+    public ResponseEntity<Object> deletePais(@PathVariable(value = "id") Long id) {
+ 	return ResponseEntity.status(HttpStatus.NO_CONTENT).body(this.produtoService.apagarPeloId(id));
+    }
 
 }
